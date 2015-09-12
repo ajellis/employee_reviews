d0fcf5fb9f3ac16c503cf34d05c8c65a18e2ee20
Anthony Ellis <ajellis1989@gmail.com>
2015-09-12 15:44:20 -0400

    Adds employee reviews

--- department.rb
+++ department.rb
@@ -2,6 +2,7 @@ require './employee'
 class Department
   attr_reader :employees, :dep_name
 
+
   def initialize(dep_name, employees= [])
     @dep_name = dep_name
     @employees = employees
--- employee.rb
+++ employee.rb
@@ -1,11 +1,13 @@
 class Employee
   attr_reader :name, :email, :phone, :salary
+  attr_accessor :review
 
   def initialize(name, email, phone, salary)
     @name = name
     @email = email
     @phone = phone
     @salary = salary
+    @review = nil
   end
-
+ def satisfactory?
 end
